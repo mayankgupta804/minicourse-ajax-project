2324f9ca71141fc3990296560e01f2f2346472af
mayankgupta804 <mayank.gupta804@gmail.com>
2022-12-19 17:13:52 +0530

    Initial commit: Get background image from gmaps API

--- js/script.js
+++ js/script.js
@@ -6,69 +6,33 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
+    var $street = $('#street');
+    var $city = $('#city');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
+    var street, city;
 
-    $greeting.text('So, you want to live at ' + address + '?');
+    $("#street").val(function(_, value){
+        street = this.value;
+    });
+
+    $("#city").val(function(_, value){
+        city = this.value;
+    });
 
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+    var location= street + "," + city;
+    var streetViewApiKey = "iamwithstupid";
 
+    var streetviewApiPath = `http://maps.googleapis.com/maps/api/streetview?size=600x300&location=${location}&key=${streetViewApiKey}`;
 
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
-
-
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
-
-            clearTimeout(wikiRequestTimeout);
-        }
-    });
-
-    return false;
+    $body.append(`<img class="bgimg" src=${streetviewApiPath} />`);
 };
 
-$('#form-container').submit(loadData);
-
-// loadData();
+$('#form-container').submit(function(event) {
+    loadData();
+    event.preventDefault();
+});
